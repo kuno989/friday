7e901ed9debcc1235f120ddcf429092cfa58223b
kuno989 <kuno@twelvelabs.io>
2021-09-22 00:12:20 +0900

    remove agent move to new repository

--- cmd/root.go
+++ cmd/root.go
@@ -34,5 +34,4 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is logger.yml)")
 	rootCmd.AddCommand(runBackend)
 	rootCmd.AddCommand(runFridayEngine)
-	rootCmd.AddCommand(runAgent)
 }
